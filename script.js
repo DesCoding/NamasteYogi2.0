02476cf54af21526e7477a9e5e6dbb42827371f8
Des <d.stout31@yahoo.com>
2020-12-22 13:32:39 -0600

    updated mystudio html with search field/buttons and placeholders for API feeds.  added Jquery onclick event listenters for search fields and API feed is working for yoga poses.

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 var youTubeAPI = "AIzaSyAvSFR7-A7Kzgdxke72C_81WGWueciQj-8"
-// CHANGES
-var yelpLocationAPI = "BXl-oGLTGuQQ1mZjGZ3mGnAMpz8-Xp_I0dASCnxX0t9wFJNCFyh_M1Gsad-kQT7kXHOomdEt5u3nBTS4lcW7FdaTiqaPw--075rZ9jMLYX_QyVmv18DsYy4CdgncX3Yx"
-
-function findStudioNearYou(location){
-    $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=' + 'Yoga Studio' + '&location=' + location,
-        headers: {
-            "Authorization": "Bearer BXl-oGLTGuQQ1mZjGZ3mGnAMpz8-Xp_I0dASCnxX0t9wFJNCFyh_M1Gsad-kQT7kXHOomdEt5u3nBTS4lcW7FdaTiqaPw--075rZ9jMLYX_QyVmv18DsYy4CdgncX3Yx"
-        },
-        success: function(result) {
-            console.log(result)
-        var yogaBusiness = result.businesses[0].name
-        $(".yogaStudio").html(yogaBusiness)
-
-        }
-    });
-}
-//change this to get zipcode from user input. 
-var zipcode = "01073"
-findStudioNearYou(zipcode);
 
 function findYogaVideo(userVideoSearch){
     $.ajax({
@@ -37,40 +17,37 @@ function findYogaVideo(userVideoSearch){
 // var userVideoInput = "warrior pose"
 // findYogaVideo(userVideoInput);
 
+//button for yoga pose api data search - this is working! :)
 $(".btn-primary").on("click", function() {
-var userSearch = $("#userSearch").val();
-findYogaVideo(userSearch)
-})
+    var userSearch = $("#userSearch").val();
+    findYogaVideo(userSearch)
+    })
+    
 
-//create a save button and save to local storage
+    var yelpLocationAPI = "BXl-oGLTGuQQ1mZjGZ3mGnAMpz8-Xp_I0dASCnxX0t9wFJNCFyh_M1Gsad-kQT7kXHOomdEt5u3nBTS4lcW7FdaTiqaPw--075rZ9jMLYX_QyVmv18DsYy4CdgncX3Yx"
 
-           //     / Event listener for btn-primary
-            //     $(".btn-primary").on("click", function() {
-            //     // Assigning a variable for userSearch
-            //     var userSearch = $("#userSearch").val();
-            //     // Constructing a URL to search Open Weather Map API for the City,State
-            //     var cityAPISearch = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearch + ",US&appid=ae3e2e9bcf9bf274f436653d07f65b1c";
-            //     // Performing our AJAX GET request
-            //     $.ajax({
-            //             url: cityAPISearch,
-            //             method: "GET"
-            //         })
-            //         // After the data comes back from the API
-            //         .then(function(response) {
-            //             // Storing an array of results in the results variable
-            //             var results = [userSearch, response.coord.lat, response.coord.lon];
-            //             console.log(results);
-            //         });
-            // });
-            // $.ajax({
-            //     url: queryURL,
-            //     method: "GET"
-            //     //.then promises to retun the function (in this API) and captures the response
-            //    }).then(function (response) {
-            //        console.log("response: ", response);
-           
-            //        //displayFiveDayForecast(response);
-            //        for (var i = 3; i < response.list.length; i = i+8){
-            //        displayFiveDayForecast(response.list[i], response.list[i].dt_txt)
-            //        }
-            //    })
+function findStudioNearYou(location){
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=' + 'Yoga Studio' + '&location=' + location,
+        headers: {
+            "Authorization": "Bearer BXl-oGLTGuQQ1mZjGZ3mGnAMpz8-Xp_I0dASCnxX0t9wFJNCFyh_M1Gsad-kQT7kXHOomdEt5u3nBTS4lcW7FdaTiqaPw--075rZ9jMLYX_QyVmv18DsYy4CdgncX3Yx"
+        },
+        success: function(result) {
+            console.log(result)
+        var yogaBusiness = result.businesses[0].name
+        $(".zipPlaceholder").append(location)
+        $(".yogaStudio").html(yogaBusiness)
+
+        }
+    });
+}
+//commented these sections out becuase jquery button/logic will overwrite these to make the search dynamic instead of hardcode 
+// var zipcode = "01073"
+// findStudioNearYou(zipcode);
+
+//button for yoga studio location search w/ zippcode - not working right
+$(".btn-zip").on("click", function() {
+    var zipSearch = $("#zipSearch").val();
+    findStudioNearYou(zipSearch)
+    })
+
